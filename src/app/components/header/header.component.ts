5656b54795f2b5a59280618583d083f4c5d09a91
unknown <javierbiselli@gmail.com>
2023-06-10 17:53:22 -0300

    feat: added functionality to trending navigation buttons and title property to trending new text

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,6 +13,7 @@ export class HeaderComponent implements OnInit {
   closeIcon: string = "fa-xmark";
 
   ngOnInit() {
+    // clock logic
     const options: Intl.DateTimeFormatOptions = {
       weekday: "short",
       month: "short",
@@ -32,6 +33,7 @@ export class HeaderComponent implements OnInit {
     setInterval(updateDateTime, 1000);
   }
 
+  // menu logic
   menuButtonClicked: boolean = false;
   menuButtonClass: string = "fa-bars";
 
@@ -42,6 +44,7 @@ export class HeaderComponent implements OnInit {
       : (this.menuButtonClass = "fa-bars");
   }
 
+  // search bar logic
   searchButtonClicked: boolean = false;
   searchButtonClass: string = "fa-magnifying-glass";
 
@@ -52,18 +55,69 @@ export class HeaderComponent implements OnInit {
       : (this.searchButtonClass = "fa-magnifying-glass");
   }
 
+  //--------------- TYPED.JS ---------------
+
+  strings: string[] = [
+    "Probando un texto corto",
+    "Probando un texto largo muy largo para ver si esto funciona y no se pasa el texto de su contenedor",
+    "Otro texto de longitud media para probar cómo funciona",
+  ];
+  currentStringIndex: number = 0;
+  typed: Typed | undefined;
+  autoChangeInterval: any | undefined;
+
   ngAfterViewInit() {
+    this.initializeTyped();
+    this.startAutoChange();
+  }
+
+  // typed.js configuration
+  initializeTyped() {
     const options = {
-      strings: [
-        "Probando un texto corto",
-        "Probando un texto largo muy largo para ver si esto funciona y no se pasa el texto de su contenedor",
-      ],
-      typeSpeed: 50,
-      backSpeed: 20,
-      backDelay: 2500,
+      strings: [this.strings[this.currentStringIndex]],
+      typeSpeed: 20,
+      backDelay: 10000,
       loop: true,
     };
 
-    const typed = new Typed(".trendig-new-text", options);
+    this.typed = new Typed(".trending-new-text", options);
+  }
+
+  // functions for navigating between trending news
+  startAutoChange() {
+    this.autoChangeInterval = setInterval(() => {
+      this.changeTrendingNew(1);
+    }, 6000);
+  }
+
+  stopAutoChange() {
+    if (this.autoChangeInterval) {
+      clearInterval(this.autoChangeInterval);
+    }
+  }
+
+  incrementStringIndex() {
+    if (this.currentStringIndex < this.strings.length - 1) {
+      this.changeTrendingNew(1);
+    }
+  }
+
+  decrementStringIndex() {
+    if (this.currentStringIndex > 0) {
+      this.changeTrendingNew(-1);
+    }
+  }
+
+  changeTrendingNew(offset: number) {
+    this.stopAutoChange();
+
+    if (this.typed) {
+      this.typed.destroy();
+    }
+
+    this.currentStringIndex =
+      (this.currentStringIndex + offset) % this.strings.length;
+    this.initializeTyped();
+    this.startAutoChange();
   }
 }
