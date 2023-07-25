10608c1fe39af0db51601bceca81e799b0da97b5
Santiago Bartoli <sjbartoli@gmail.com>
2023-07-25 19:03:10 -0300

    Refactor: clean code comments

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,8 +22,6 @@ export class AuthService {
     this.userSubject = new BehaviorSubject<User | null>(
       JSON.parse(localStorage.getItem('currentUser') || 'null')
     );
-    console.log('userSubject value:'); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-    console.log(this.userSubject); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
     this.user = this.userSubject.asObservable(); // Ver si se usa en algun lado
   }
 
@@ -38,31 +36,20 @@ export class AuthService {
     let user = this.dataHandlingService.users.getValue()
                 .find(x => x.email === email && x.password === password);
                 // .find(({email}) => email === email);
-    console.log('user found'); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-    console.log(user); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
 
     userObservable = new BehaviorSubject<User | null>(user || null);
     return userObservable.asObservable()
       .pipe(map(user => {
         // store user details and jwt token in local storage to keep user logged in between page refreshes
         localStorage.setItem('currentUser', JSON.stringify(user));
-        console.log('local storage set'); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-        console.log(localStorage.getItem('currentUser')); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGG
-        console.log('userSubject value:'); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-        console.log(this.userSubject); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
         this.userSubject.next(user);
         return user;
       }));
   }
 
   logout() {
-    console.log('logging out') // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
     localStorage.removeItem('currentUser');
     this.userSubject.next(JSON.parse('null'));
     this.router.navigate(['/']);
-    console.log('userSubject value:'); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-    console.log(this.userSubject); // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-    console.log('logging out success') // CONSOLE LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
-    
   }
 }
