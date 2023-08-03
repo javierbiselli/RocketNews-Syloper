fa70d6433dc957fa4fdb6f0ab7011c6cc289808d
Javier Biselli <101212526+javierbiselli@users.noreply.github.com>
2023-08-02 21:51:15 -0300

    fix: fixed trendings not changing automatically

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -60,6 +60,7 @@ export class HeaderComponent implements OnInit {
       }
     );
     this.initializeTyped();
+    this.startAutoChange();
 
     // clock logic
     const options: Intl.DateTimeFormatOptions = {
